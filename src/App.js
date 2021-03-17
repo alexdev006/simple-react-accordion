90606a66d973eafb8c18a7466d34a9d1b2423cc9
AlexG <alexguidat@gmail.com>
2021-03-17 09:42:45 +0100

    modif titre appli

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { accordionData } from './utils/content';
 const App = () => {
   return (
     <div>
-      <h1>React Accordion Demo</h1>
+      <h1>Simple react accordion component</h1>
       <div className="accordion">
         {accordionData.map(({ title, content }) => (
           <Accordion title={title} content={content} />
